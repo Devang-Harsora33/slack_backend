f841e897ea7ce2b21ef7d19e55509bb014c7260e
SolutionsbyDevHive <devangkh1206@gmail.com>
2024-08-25 22:18:49 +0530

    update

--- server.mjs
+++ server.mjs
@@ -1,12 +1,15 @@
-const { App } = require("@slack/bolt");
+import pkg from "@slack/bolt";
+const { App } = pkg;
+import dotenv from "dotenv";
+// import { globalNewTaskCallback } from "./actions.js";
 
-// Initialize your Bolt app
+dotenv.config();
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// Respond to a global shortcut with a simple "Hello" modal
 app.shortcut("global_new_task", async ({ shortcut, ack, client }) => {
   try {
     await ack(); // Acknowledge the shortcut
@@ -41,7 +44,6 @@ app.shortcut("global_new_task", async ({ shortcut, ack, client }) => {
   }
 });
 
-// Start the app
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log("⚡️ Bolt app is running!");
